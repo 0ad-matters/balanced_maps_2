41f0be4fe87b62c6fd1ad45ceb5ceae959aeea58
andy5995 <andy400-dev@yahoo.com>
2021-10-20 00:02:44 -0500

    show filter as "Balanced Maps 2"
    Signed-off-by: andy5995 <andy400-dev@yahoo.com>

--- gui/maps/MapFilters_badmod_2.js
+++ gui/maps/MapFilters_badmod_2.js
@@ -1,7 +1,7 @@
 MapFilters.prototype.Filters.push(
 	{
 		"Name": "Balanced Maps 2",
-		"Title": "badmod",
+		"Title": "Balanced Maps 2",
 		"Description": "Balanced Maps 2",
 		"Match": ["badmod"]
 	});
